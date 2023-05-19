c938e3ab8e7b3d92acd96c9d099c3242ccffd37e
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2023-05-19 15:45:52 +0530

    created edit api and frontend

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,11 +2,12 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Profile from "@components/profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
   const [post, setPost] = useState([]);
@@ -21,11 +22,11 @@ const MyProfile = () => {
     if (session?.user.id) fetchPost();
   }, []);
 
-  const handleEdit = () => {
-    
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = () => {};
+  const handleDelete = (post) => {};
 
   return (
     <div>
